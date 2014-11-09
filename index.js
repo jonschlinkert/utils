c24d6dbda979a18ffb7edbdf49ba6f4a917a204e
jonschlinkert <dev@sellside.com>
2014-11-09 04:03:12 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * utils <https://github.com/jonschlinkert/utils>
+ *
+ * Copyright (c) 2014 Jon Schlinkert, contributors.
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function () {
+  // do stuff
+};
