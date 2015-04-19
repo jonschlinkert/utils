2f1981f044c10f5a6dd6478a7a4dfec2e044b9f2
jonschlinkert <jon.schlinkert@sellside.com>
2015-04-19 16:11:48 -0400

    update helpers/tasks

--- verbfile.js
+++ verbfile.js
@@ -1,33 +1,28 @@
 'use strict';
 
 var fs = require('fs');
+var path = require('path');
+var util = require('util');
 var gutil = require('gulp-util');
 var plugin = require('./support/plugins');
 var istanbul = require('gulp-istanbul');
-var stripAnsi = require('strip-ansi');
 var jshint = require('gulp-jshint');
 var mocha = require('gulp-mocha');
 var verb = require('verb');
 
-// template helper to include coverage report
-verb.helper('coverage', function (fp) {
-  var str = fs.readFileSync(fp, 'utf8');
-  return stripAnsi(str).replace(/^=.*/gm, '');
+verb.helper('methods', function (fp) {
+  var obj = require(path.resolve(process.cwd(), fp));
+  return util.inspect(obj, null, 10);
 });
 
-// ignore patterns for excluding TOC headings (for verb's built-in `toc` helper)
-verb.option('toc.ignore', ['Install', 'Author', 'License']);
-
-// generate the README
-verb.task('readme', ['test'], function () {
-  return verb.src('.verb.md')
+verb.task('readme', function () {
+  verb.src(['.verb.md', 'docs/*.md'])
     .pipe(verb.dest('.'));
 });
 
 verb.task('lint', function () {
-  return verb.src(['index.js', 'lib/**/*.js'])
+  verb.src(['index.js', 'lib/**/*.js'])
     .pipe(jshint('.jshintrc'))
-    /* deps:jshint-stylish */
     .pipe(jshint.reporter('jshint-stylish'));
 });
 
@@ -38,7 +33,6 @@ verb.task('test', ['lint'], function (cb) {
     .on('finish', function () {
       verb.src(['test/*.js'])
         .pipe(mocha())
-        .on('error', gutil.log)
         .pipe(istanbul.writeReports({
           reporters: [ 'text' ],
           reportOpts: {dir: 'coverage', file: 'summary.txt'}
