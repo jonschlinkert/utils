5018698a75aa9bffa783d538b1cba59b19a3ab70
jonschlinkert <jon.schlinkert@sellside.com>
2015-09-23 20:06:49 -0400

    adds gulp

--- verbfile.js
+++ verbfile.js
@@ -2,12 +2,8 @@
 
 var path = require('path');
 var util = require('util');
-var plugin = require('./support/plugins');
-var stylish = require('jshint-stylish');
-var istanbul = require('gulp-istanbul');
-var jshint = require('gulp-jshint');
-var mocha = require('gulp-mocha');
 var verb = require('verb');
+var plugin = require('./support/plugins');
 
 verb.helper('utils_methods', function (fp) {
   var obj = require(path.resolve(process.cwd(), fp));
@@ -15,31 +11,8 @@ verb.helper('utils_methods', function (fp) {
 });
 
 verb.task('readme', function () {
-  verb.src(['.verb.md', 'docs/*.md'])
+  return verb.src(['.verb.md', 'docs/*.md'])
     .pipe(verb.dest('.'));
-});
-
-var lint = ['index.js', 'lib/**/*.js'];
-
-verb.task('coverage', function () {
-  return verb.src(lint)
-    .pipe(istanbul())
-    .pipe(istanbul.hookRequire());
-});
-
-verb.task('mocha', ['coverage'], function () {
-  return verb.src('test/*.js')
-    .pipe(mocha({reporter: 'spec'}))
-    .pipe(istanbul.writeReports({
-      reporters: [ 'text' ],
-      reportOpts: {dir: 'coverage', file: 'summary.txt'}
-    }))
-});
-
-verb.task('jshint', function () {
-  return verb.src(lint)
-    .pipe(jshint())
-    .pipe(jshint.reporter(stylish));
 });
 
 verb.task('names', function () {
@@ -54,4 +27,4 @@ verb.task('modularize', function () {
     .pipe(verb.dest('lib/temp'));
 });
 
-verb.task('default', ['mocha', 'jshint', 'readme']);
+verb.task('default', ['readme']);
